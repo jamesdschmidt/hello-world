6d26efd2bb46d146a4e2650bc817b7a42d93ddd5
James Schmidt <jschmidt@overstock.com>
2020-06-17 16:59:32 -0600

    Add controller

---
+++ src/test/java/com/example/ControllerTests.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class ControllerTests {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  void shouldReturnHelloWorld() throws Exception {
+    mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+        .andExpect(content().string("hello, world"));
+  }
+}
